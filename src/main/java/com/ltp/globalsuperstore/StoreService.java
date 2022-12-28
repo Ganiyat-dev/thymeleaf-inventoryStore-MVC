84b47cc2c71e939ab271b41087676ae4a67e259f
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2022-12-28 16:08:30 +0100

    chore: Implementing CRUD operations using the MVC, annotations and thymeleaf

---
+++ src/main/java/com/ltp/globalsuperstore/StoreService.java
@@ -0,0 +1,22 @@
+// package com.ltp.globalsuperstore;
+
+// import java.util.List;
+
+// public class StoreService {
+
+//     StoreRepository storeRepository = new StoreRepository();
+
+//     public Item getItem(int index) {
+//         return storeRepository.getItem(index);
+//     }
+
+//     public void addItem(Item item) {
+//         storeRepository.addItem(item);
+//     }
+//     public void updateItem(Item item, int index) {
+//         storeRepository.updateItem(item, index);
+//     }
+// public List<Item> getItems() {
+//     return storeRepository.getItems();
+// }
+// }
